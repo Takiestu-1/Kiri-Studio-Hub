f96b27a855e02252cafd5b27f59c4837691096fb
Takiestu-1 <162549109+Takiestu-1@users.noreply.github.com>
2024-03-07 16:57:44 +0700

    Update xemsexitthoi.lua

--- xemsexitthoi.lua
+++ xemsexitthoi.lua
@@ -1,6 +1,5 @@
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
-local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/Takiestu-1/Kiri-Studio-Hub/main/02148291421412-1.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/Takiestu-1/Kiri-Studio-Hub/main/02148291421412-2.lua"))()
 --------------------------------------------------------------------------------------------------------------------------------------------
 local Window = Fluent:CreateWindow({
     Title = "Kiri Studio",
